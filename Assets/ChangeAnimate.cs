064271ab42948dd68cb602f97bc030e5e55c888d
Nutthakorn043 <yonpinw084@gmail.com>
2022-02-27 16:06:13 +0700

    add file

---
+++ Assets/ChangeAnimate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimate : MonoBehaviour
+{
+    public void getAnimatorDefeate() {
+        this.GetComponent<Animator>().Play("Silly Dancing");
+    }
+    public void getAnimatorRumba() {
+        this.GetComponent<Animator>().Play("mixamo_com");
+    }
+}
